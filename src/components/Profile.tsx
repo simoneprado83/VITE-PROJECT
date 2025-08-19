f637262821a254a316e119311a8f381b0174aac1
simoneprado83 <si.prado83@gmail.com>
2025-08-19 20:47:37 -0300

    Atualiza estilos e componente Profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import "../assets/react.svg"
+function Profile() {
+  return (
+    <>
+      <a href="#">
+        <img 
+          src="https://media.licdn.com/dms/image/v2/C5103AQHZS0zDo97ZKA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1517606146956?e=1758758400&v=beta&t=oDvebyKBVxjWcPodWqKfokLVl6p5Pw-5Ku15w6myys8" 
+          alt="Simone Prado"
+        />
+      </a>
+      <h3>Simone Veiga Ribeiro Prado</h3>
+      <p>Desenvolvedora Front-end</p>
+    </>
+  )
+}
+
+export default Profile
